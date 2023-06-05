0fc6a6c0b6c55d5eae50261ccaf51aff7749f972
Ivan Noleto <ivannoleto@hotmail.com>
2023-06-05 16:01:49 -0300

    ..

--- app/routes/animal.py
+++ app/routes/animal.py
@@ -28,6 +28,12 @@ async def get_animal_by_id(brinco: int, db: Session = Depends(get_db)):
     return animal.get_byid(brinco, db)
 
 
+@router.get("/animal/brinco/{brinco}")
+async def get_animal_by_id(brinco: int, db: Session = Depends(get_db)):
+    """Retorna um animal pelo brinco."""
+    return animal.get_byid(brinco, db)
+
+
 @router.patch("/animal/{animal_id}")
 async def update_animal(animal_id: int, item: AnimalCreate,
                         db: Session = Depends(get_db)):
