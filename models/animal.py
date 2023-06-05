6470db041d906451033fbba9fb47b30f7dc2af25
Ivan Noleto <ivannoleto@hotmail.com>
2023-06-04 23:18:51 -0300

    Mapped para os tipos

--- models/animal.py
+++ models/animal.py
@@ -7,7 +7,7 @@ from models.base import Base
 class Animal(Base):
     __tablename__ = 'animal'
 
-    id: int = Column(Integer, primary_key=True, autoincrement=True)
+    id: Mapped[int] = Column(Integer, primary_key=True, autoincrement=True)
     chip = Column(Integer)
     brinco = Column(Integer)
     origem = Column(String)
