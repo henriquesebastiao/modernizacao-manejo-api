0cf8be87a097d432feae28d384f0a518ae1cdd31
Ivan Noleto <ivannoleto@hotmail.com>
2023-06-19 23:13:17 -0300

    test 3

--- app/routes/raca.py
+++ app/routes/raca.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 
+from app.database import get_session
 from app.schemas.raca import RacaCreate, RacaSchema, RacaUpdate
 from app.services.raca import RacaService
 
@@ -7,26 +8,30 @@ router = APIRouter(prefix="/raca", tags=["Raca"])
 
 
 @router.post("/", response_model=RacaSchema, status_code=201)
-async def create(cargo: RacaCreate, service=Depends(RacaService)):
-    return service.create(cargo)
+async def create(cargo: RacaCreate, session=Depends(get_session)):
+    service = RacaService(session)
+    return await service.create(cargo)
 
 
 @router.get("/{raca_id}", response_model=RacaSchema)
-def get_by_id(raca_id: int, service=Depends(RacaService)):
-    return service.get(raca_id)
+async def get_by_id(raca_id: int, session=Depends(get_session)):
+    service = RacaService(session)
+    return await service.get_by_id(raca_id)
 
 
 @router.get("/", response_model=list[RacaSchema])
-async def get_all(service=Depends(RacaService)):
-    return service.get_all()
+async def get_all(session=Depends(get_session)):
+    service = RacaService(session)
+    return await service.get_all()
 
 
 @router.patch("/{raca_id}")
 async def update(raca_id: int, raca: RacaUpdate,
                  service=Depends(RacaService)):
-    return service.update(raca_id, raca)
+    return await service.update(raca_id, raca)
 
 
 @router.delete("/{raca_id}")
-async def delete(raca_id: int, service=Depends(RacaService)) -> dict:
-    return service.delete(raca_id)
+async def delete(raca_id: int, session=Depends(get_session)) -> dict:
+    service = RacaService(session)
+    return await service.delete(raca_id)
