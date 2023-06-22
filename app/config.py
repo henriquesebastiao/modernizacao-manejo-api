355ef8f07edc7d7131c23da8429ef6f6622e1977
Ivan Noleto <ivannoleto@hotmail.com>
2023-06-22 02:57:09 -0300

    test 5

--- app/config.py
+++ app/config.py
@@ -2,13 +2,13 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_type: str
-    db_user: str
-    db_password: str
-    db_host: str
-    db_port: int
-    db_name: str
-    db_ssl: str
+    db_type: str | None
+    db_user: str | None
+    db_password: str | None
+    db_host: str | None
+    db_port: int | None
+    db_name: str | None
+    db_ssl: str | None
 
     class Config:
         env_file = ".env"
