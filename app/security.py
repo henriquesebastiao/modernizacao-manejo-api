1b1c362ea930a2b67d15d7cc856c3d760a29910d
Ivan Noleto <ivannoleto@hotmail.com>
2023-06-26 19:03:24 -0300

    fix

--- app/security.py
+++ app/security.py
@@ -52,7 +52,8 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(
+            minutes=settings.access_token_expire_minutes)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.secret_key,
                              algorithm=settings.algorithm)
