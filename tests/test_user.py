6ce75b2cc76c01aec65ececbe3b1022028b41d06
Henrique Sebastião <henriquesebastiao.web@gmail.com>
2023-08-22 14:04:21 -0400

    Iniciando com testes + test branch dev

---
+++ tests/test_user.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+def test_create_user_deve_retornar_201():
+    client = TestClient(app)
+
+    response = client.post(
+        '/user/',
+        json={
+            'first_name': 'Test',
+            'last_name': 'Pytest',
+            'email': 'test@pytest.com',
+            'password': 'segura123',
+        },
+    )
+
+    assert response.status_code == 201
+    # Criar assert com JSON
