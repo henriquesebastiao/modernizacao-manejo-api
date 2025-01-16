b6e779df35fc2f2b2f927898bbdda3b425d2947d
Henrique Sebastião <contato@henriquesebastiao.com>
2025-01-16 06:08:02 -0400

    👨🏻‍🔧 fix: Fixing script that starts containerized application

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-export OTEL_SERVICE_NAME=app-manejo
-export OTEL_EXPORTER_OTLP_ENDPOINT=tempo-manejo:4317
-export OTEL_EXPORTER_OTLP_INSECURE=true
-export OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true
-export OTEL_TRACES_EXPORTER=otlp
-export OTEL_METRICS_EXPORTER=otlp
-export OTEL_LOGS_EXPORTER=otlp
-export PYTHON_COLORS=0
-
 export TEST=0
 
 alembic upgrade head
-opentelemetry-instrument python app/main.py
+python app/main.py
